0129607993585db0a49e2ff37226a9cf6c10838b
fuwzie <ixhardscoper@gmail.com>
2023-12-27 10:49:16 +0100

    Kommenterade kod + fixade litet sql fel

--- MiB Projektet/src/mib/LosenordsandringAlien.java
+++ MiB Projektet/src/mib/LosenordsandringAlien.java
@@ -138,35 +138,45 @@ public class LosenordsandringAlien extends javax.swing.JFrame {
     }//GEN-LAST:event_pwGammaltLosenordActionPerformed
 
     private void btnBytLosenordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBytLosenordActionPerformed
+        //Kollar så alla textrutor har ett värde.
         if (Validering.textFaltHarVarde(pwGammaltLosenord) && Validering.textFaltHarVarde(pwNyttLosenord) && Validering.textFaltHarVarde(pwNyttLosenordUpprepa))
         { try {
+             
             String kollaLosenord = new String(pwGammaltLosenord.getPassword());
+            
+            //Hämtar ut användarens nuvarande lösenord
             String sqlFraga = "SELECT losenord FROM alien where alien_id = " + id;
             String sqlSvar = idb.fetchSingle(sqlFraga);
             
             if (sqlSvar.equals(kollaLosenord)) {
+                //Metoden jämför inmatade gamla lösenordet med svaret från databasen
+                
+                //Hämtar ut värden på de nya lösenorden som matats in
                 String nyttLosenord = new String(pwNyttLosenord.getPassword());
                 String nyttLosenordUpprepa = new String (pwNyttLosenordUpprepa.getPassword());
-                
-                if (!nyttLosenord.equals(kollaLosenord))
+                if (!nyttLosenord.equals(sqlSvar))
                 {
                     //Kollar så nytt lösenord ej är samma som tidigare.
                 
                 if (nyttLosenord.equals(nyttLosenordUpprepa)){
-         
-                    String bytLosenord = "UPDATE alien set losenord = " + nyttLosenord + " where alien_id = " + id;
+                    //Om lösenordet upprepats korrekt och inget annat fel uppstått tidigare så uppdaterar den lösenordet.
+                    String bytLosenord = "UPDATE alien set losenord = '" + nyttLosenord + "' where alien_id = " + id;
                     idb.update(bytLosenord);
+                    JOptionPane.showMessageDialog(null, "Ditt lösenord har ändrats.");
                 }
                 else {
+                    //Om nyttLosenord != nyttLosenordUpprepa
                     JOptionPane.showMessageDialog(null, "Nytt lösenord matchar inte upprepning!");
                 }
                 } else {
+                    //Om nyttLosenord = kollaLosenord
                     JOptionPane.showMessageDialog(null, "Nytt lösenord kan inte vara samma som tidigare.");
                 } 
             } }
         
         catch (InfException ex){
             JOptionPane.showMessageDialog(null, "Ange rätt lösenord!");
+            System.out.println("Internt felmeddelande: " + ex.getMessage());
         }
         }
     }//GEN-LAST:event_btnBytLosenordActionPerformed
