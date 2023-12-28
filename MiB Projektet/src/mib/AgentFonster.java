3d376b762c162d6b28083c999c90bd2894b5423b
fuwzie <ixhardscoper@gmail.com>
2023-12-28 22:30:35 +0100

    Skapade RegistreraUtrustning med en fungerande registreringsmetod och länkar till denna klass från AgentFonster klassen

--- MiB Projektet/src/mib/AgentFonster.java
+++ MiB Projektet/src/mib/AgentFonster.java
@@ -206,7 +206,7 @@ public class AgentFonster extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        new RegistreraUtrustning(idb).setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
